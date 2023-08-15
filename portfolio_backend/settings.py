d604ce6cd19898568a9200ce5f9312f9d47c78d2
Kelviz <mysviurch@gmail.com>
2023-08-15 13:34:10 +0100

    update

--- portfolio_backend/settings.py
+++ portfolio_backend/settings.py
@@ -142,6 +142,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "https://urchdev.netlify.app",
+    "https://portfolio-frontend-mocha-gamma.vercel.app",
 ]
 
 
